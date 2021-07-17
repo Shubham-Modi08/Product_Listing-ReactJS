e03f38441df8eabe767a4c93da3f4d5bfcd48890
shubh <mailshubham8995@gmail.com>
2021-07-17 13:42:52 +0530

    Completed the css

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,16 +1,14 @@
-import React from 'react';
+
+import React from "react";
 
 const Header = () => {
-        return(
-            <div className="ui fixed menu">
-                <div  className = "ui container center">
-                    <h2>Fibonacci Day Sale</h2>
-                </div>
-
-
-            </div>
-        );
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>FIBONACCI DAY SALE</h2>
+      </div>
+    </div>
+  );
 };
-
 
 export default Header;
